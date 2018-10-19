08e60268c92c7b6478f33cbedc8f00f35a0d965a
f24e3edbeb91f8e3c850874b6806432241f1820a <samanthagmccormick@users.noreply.github.com>
2018-10-19 15:23:24 -0600

    Comment

--- src/main/java/WeatherStation.java
+++ src/main/java/WeatherStation.java
@@ -8,6 +8,7 @@ public class WeatherStation {
 
     CurrentConditionsDisplay currentConditionsDisplay = new CurrentConditionsDisplay(weatherData);
 
+    // Calling this lets the observers know that the data has been updated. It also calls display()
     weatherData.setMeasurements(80, 65, 30f);
   }
 }
